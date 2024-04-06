71c6b15cd893e7344aeb9f836094c3f52f903cab
YesNile <holmovdanil2@gmail.com>
2024-04-07 03:04:20 +0500

    added api-endpoimt

--- ml/main.py
+++ ml/main.py
@@ -1,62 +1,61 @@
-import os
-from time import sleep
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import cv2
+import numpy as np
+import io
 from stitching import Stitcher
-import cv2 as cv
 import matplotlib.pyplot as plt
-from celery import Celery
 
+app = Flask(__name__)
+CORS(app)
 
-# settings = {
-#     "detector": "sift", 
-#     "confidence_threshold": 0.4,
-#     "blend_strength": 5,
-#     "wave_correct_kind": "vert",
-#     "nfeatures":5000,
-#     "warper_type":"plane",
-#     "matcher_type":"affine",
-#     "match_conf":0.5,
-#     "blender_type":"multiband",
-#     "finder":"dp_colorgrad",
-#     "adjuster":"ray",
-# }
+def process_images(image1, image2):
+    settings = {
+        "detector": "sift", 
+        "confidence_threshold": 1e-6,
+        "finder": "dp_color",
+        "adjuster": "ray",
+        "warper_type": "plane",
+        "block_size": 10,
+        "nfeatures": 15000,
+        "match_conf": 0.5,
+        "wave_correct_kind": "auto",
+        "compensator": "gain_blocks",
+        "matches_graph_dot_file": "matches_graph.dot"
+    }
 
-settings={
-    "detector": "sift", 
-    "confidence_threshold": 1e-6,
-    "finder":"dp_color",
-    "adjuster":"ray",
-    "warper_type":"plane",
-    "block_size":10,
-    "nfeatures":15000,
-    "match_conf":0.5,
-    "wave_correct_kind": "auto",
-    "compensator": "gain_blocks",
-    "matches_graph_dot_file":"matches_graph.dot" #To plot graph type in terminal| dot -Tpng matches_graph.dot -o matches_graph.png
+    stitcher = Stitcher(**settings)
+    panorama = stitcher.stitch([image1, image2])
 
-}
+    def plot_image(img, figsize_in_inches=(5, 5), dpi=300):
+        fig, ax = plt.subplots(figsize=figsize_in_inches, dpi=dpi)
+        ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        ax.axis('off')
+        plt.tight_layout(pad=0)
+        img_bytes = io.BytesIO()
+        plt.savefig(img_bytes, format='png', dpi=dpi, bbox_inches='tight', pad_inches=0)
+        plt.close(fig)
+        img_bytes.seek(0)
+        return img_bytes
 
-stitcher = Stitcher(**settings)
-panorama = stitcher.stitch(["images/ver_top.jpeg", "images/ver_bottom.jpeg"])
-# panorama = stitcher.stitch(["images/hor_left.jpeg", "images/hor_right.jpeg"])
-# panorama = stitcher.stitch(["images/last_left.jpeg", "images/last_right.jpeg"])
+    img_bytes = plot_image(panorama)
+    return img_bytes
 
-def plot_image(img, figsize_in_inches=(5,5), dpi=300):
-    fig, ax = plt.subplots(figsize=figsize_in_inches, dpi=dpi)
-    
-    # Поворот изображения на 90 градусов вправо
-    # img_rotated = cv.rotate(img, cv.ROTATE_90_CLOCKWISE)
+@app.route('/api/process-images', methods=['POST'])
+def process_images_api():
+    # Получение загруженных изображений из запроса
+    image1 = request.files['image1'].read()
+    image2 = request.files['image2'].read()
 
-    ax.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
-    
-    # Удаление координатных осей
-    ax.axis('off')
-    
-    # Удаление отступов вокруг изображения
-    plt.tight_layout(pad=0)
-    
-    # Сохранение изображения с высоким качеством
-    plt.savefig('image_file.png', dpi=dpi, bbox_inches='tight', pad_inches=0)
-    
-    # Закрытие фигуры для освобождения ресурсов
-    plt.close(fig)
-plot_image(panorama)
+    # Преобразование изображений из бинарных данных в объекты OpenCV
+    image1 = cv2.imdecode(np.fromstring(image1, np.uint8), cv2.IMREAD_COLOR)
+    image2 = cv2.imdecode(np.fromstring(image2, np.uint8), cv2.IMREAD_COLOR)
+
+    # Обработка изображений с помощью вашего кода
+    processed_image_bytes = process_images(image1, image2)
+
+    # Отправка обработанного изображения обратно клиенту
+    return send_file(processed_image_bytes, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run()
